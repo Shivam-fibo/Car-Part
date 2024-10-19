b9dc9833cbbc1e1caacb75d43ec9a8f83ec033d6
Shivam <sg641818@gmail.com>
2024-10-19 17:40:09 +0530

    make resonsive layout for search parts

--- Frontend/src/components/CarPartsList.jsx
+++ Frontend/src/components/CarPartsList.jsx
@@ -25,12 +25,12 @@ const CarPartsList = ({ parts }) => {
   const selectedPartData = parts.find(part => part.id === selectedPart);
 
   return (
-    <div className="flex items-center mb-24 bg-gray-800 p-4 rounded-lg shadow-lg transform transition-transform hover:scale-105 max-w-3xl mx-auto mt-10">
+    <div className="flex flex-col md:flex-row items-center mb-24 bg-gray-800 p-4 rounded-lg shadow-lg transform transition-transform hover:scale-105 max-w-3xl mx-auto mt-10 space-y-4 md:space-y-0 md:space-x-4">
       {/* Dropdown for selecting a part */}
       <select
         value={selectedPart}
         onChange={handlePartChange}
-        className="p-3 border border-gray-300 rounded-lg mr-4 bg-white shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="p-3 border border-gray-300 rounded-lg bg-white shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
         <option value="">Select a part...</option>
         {parts.map((part) => (
@@ -44,7 +44,7 @@ const CarPartsList = ({ parts }) => {
       <select
         value={selectedItem}
         onChange={handleItemChange}
-        className={`p-3 border border-gray-300 rounded-lg mr-4 bg-white shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500 ${!selectedPart ? 'opacity-50' : ''}`}
+        className={`p-3 border border-gray-300 rounded-lg bg-white shadow-inner focus:outline-none focus:ring-2 focus:ring-blue-500 ${!selectedPart ? 'opacity-50' : ''}`}
         disabled={!selectedPart}
       >
         <option value="">Select an item...</option>
@@ -56,7 +56,7 @@ const CarPartsList = ({ parts }) => {
       </select>
 
       <button
-        className="bg-gradient-to-r from-blue-500 to-blue-500 text-white px-6 py-3 rounded-lg shadow-md hover:shadow-lg hover:from-blue-600 hover:to-blue-600 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-300 ease-in-out"
+        className="bg-gradient-to-r from-blue-500 to-blue-600 text-white px-6 py-3 rounded-lg shadow-md hover:shadow-lg transition-all duration-300 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed"
         disabled={!selectedItem}
         onClick={handleSearch}
       >
